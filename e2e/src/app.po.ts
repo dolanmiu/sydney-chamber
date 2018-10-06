7c421d22bcb64a64545e638567a836215d52608f
Dolan Miu <dolan_miu@hotmail.com>
2018-10-06 20:08:09 +0100

    Initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,12 @@
+import { browser, by, element, promise } from 'protractor';
+
+export class AppPage {
+  // tslint:disable-next-line:no-any
+  public navigateTo(): promise.Promise<any> {
+    return browser.get('/');
+  }
+
+  public getParagraphText(): promise.Promise<string> {
+    return element(by.css('app-root h1')).getText();
+  }
+}
