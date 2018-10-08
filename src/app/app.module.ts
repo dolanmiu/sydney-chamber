e853b14f8570adfa4a7197a660e386929c943e7a
Dolan <dolan_miu@hotmail.com>
2018-10-08 02:37:36 +0100

    Add footer and nav bar scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterModule } from './footer/footer.module';
+import { NavBarModule } from './nav-bar/nav-bar.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, NavBarModule, FooterModule],
     providers: [],
     bootstrap: [AppComponent],
 })
