5617ff92d79290a0d737abfa9c9b9116fc08f14b
Dolan <dolan_miu@hotmail.com>
2018-10-08 02:48:59 +0100

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { async, TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
+import { FooterModule } from './footer/footer.module';
+import { NavBarModule } from './nav-bar/nav-bar.module';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [AppComponent],
+            imports: [FooterModule, NavBarModule],
         }).compileComponents();
     }));
 
@@ -12,18 +16,5 @@ describe('AppComponent', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
-    });
-
-    it(`should have as title 'sydney-chamber'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('sydney-chamber');
-    });
-
-    it('should render title in a h1 tag', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('Welcome to sydney-chamber!');
     });
 });
