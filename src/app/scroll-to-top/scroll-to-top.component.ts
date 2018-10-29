4974e7021581e308c366d34824ba56ba28e4753e
Dolan <dolan_miu@hotmail.com>
2018-10-29 03:13:57 +0000

    Inverted back to top when scrolling through footer

--- src/app/scroll-to-top/scroll-to-top.component.ts
+++ src/app/scroll-to-top/scroll-to-top.component.ts
@@ -9,10 +9,19 @@ export class ScrollToTopComponent {
     @HostBinding('class.unscrolled')
     public unscrolled = true;
 
+    @HostBinding('class.inverted')
+    public inverted = true;
+
     constructor(renderer: Renderer2) {
         renderer.listen('window', 'scroll', () => {
             const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
             this.unscrolled = scrollTop < 1000 ? true : false;
+
+            const pageHeight = document.documentElement.scrollHeight || document.body.scrollHeight || 0;
+            const windowHeight = document.documentElement.offsetHeight || document.body.offsetHeight;
+            const windowHeightPlusPageHeight = scrollTop + windowHeight;
+
+            this.inverted = windowHeightPlusPageHeight > pageHeight - 410;
         });
     }
 }
