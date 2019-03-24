d0dd846eb15c7955dd77bb35da934bd40569dfe3
Dolan Miu <dolan_miu@hotmail.com>
2019-03-24 22:59:33 +0000

    Add show variable

--- src/app/home/musicians/musician-card/musician-card.component.ts
+++ src/app/home/musicians/musician-card/musician-card.component.ts
@@ -7,8 +7,12 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MusicianCardComponent {
+    public show: boolean;
+
     @Input()
     public name: String;
 
-    constructor() {}
+    constructor() {
+        this.show = false;
+    }
 }
