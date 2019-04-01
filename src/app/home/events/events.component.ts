765aa5c76e1e4d0eb2bbec6dab5c9f6ebef08f22
Dolan Miu <dolan_miu@hotmail.com>
2019-04-01 19:55:33 +0100

    Add limit on events shown

--- src/app/home/events/events.component.ts
+++ src/app/home/events/events.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map, mergeMap, filter, scan } from 'rxjs/operators';
 
 import { EventsService } from '../services/events.service';
 
@@ -13,7 +14,14 @@ export class EventsComponent implements OnInit {
     public selectedEvent: EventPackage;
 
     constructor(eventsService: EventsService) {
-        this.events$ = eventsService.Events;
+        this.events$ = eventsService.Events.pipe(
+            map((events) => {
+                const [one, two, three] = events;
+
+                return [one, two, three];
+            }),
+            map((x) => x.filter((y) => !!y)),
+        );
     }
 
     public ngOnInit(): void {}
