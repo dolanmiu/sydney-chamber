5c78411c3f09f030f86f50e763a3b2ffec51630f
Dolan Miu <dolan_miu@hotmail.com>
2019-04-09 01:56:02 +0100

    Fix tests

--- src/app/home/splash/splash.component.spec.ts
+++ src/app/home/splash/splash.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { SharedModule } from 'src/app/shared/shared.module';
+
 import { ServicesModule } from '../services/services.module';
 import { BottomComponent } from './bottom/bottom.component';
 import { HeroImageComponent } from './hero-image/hero-image.component';
@@ -11,7 +13,7 @@ describe('SplashComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [ServicesModule],
+            imports: [ServicesModule, SharedModule],
             declarations: [SplashComponent, HeroImageComponent, BottomComponent],
         }).compileComponents();
     }));
