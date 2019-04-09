a8177f4b86b637219cd7820b0bb70e50f0877325
Dolan Miu <dolan_miu@hotmail.com>
2019-04-09 01:43:13 +0100

    Styling improvements

--- src/app/home/splash/splash.module.ts
+++ src/app/home/splash/splash.module.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from 'src/app/shared/shared.module';
+
 import { ServicesModule } from '../services/services.module';
 import { BottomComponent } from './bottom/bottom.component';
 import { HeroImageComponent } from './hero-image/hero-image.component';
 import { SplashComponent } from './splash.component';
 
 @NgModule({
-    imports: [CommonModule, ServicesModule],
+    imports: [CommonModule, ServicesModule, SharedModule],
     declarations: [SplashComponent, HeroImageComponent, BottomComponent],
     exports: [SplashComponent],
 })
