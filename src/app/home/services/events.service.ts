f76c6a512de6ed633b9c54209b64f9aea580d818
Dolan Miu <dolan_miu@hotmail.com>
2019-08-12 20:48:10 +0100

    Add hardcoded temporary event

--- src/app/home/services/events.service.ts
+++ src/app/home/services/events.service.ts
@@ -13,6 +13,42 @@ export class EventsService {
             .pipe(
                 map((res) => res.events),
                 map((events) => events.filter((e) => e.event.status === 'live')),
+                map((events) => [
+                    {
+                        event: {
+                            name: {
+                                text: 'Close Quarters @ The Chamber Project',
+                                html: 'string',
+                            },
+                            start: {
+                                local: new Date(2019, 8, 5, 18, 30),
+                            },
+                            url: 'https://asq.com.au/whats-on/close-quarters-the-chamber-project/',
+                            summary:
+                                // tslint:disable-next-line:max-line-length
+                                'We’re thrilled to team up with The Chamber Project to present this special ASQ Close Quarters event in Sydney! Merging classical music with urban life, The Chamber Project brings extraordinary musical experiences to a unique, familiar setting. Together with Soultrap, we offer the best way to experience this music—close up, well nourished, in comfort. In this relaxed concert we’ll tingle your auditory taste-buds with a diverse selection short works and movements currently under our fingers.',
+                        },
+                        venue: {
+                            address: {
+                                address_1: 'string',
+                                address_2: 'string',
+                                city: 'string',
+                                region: 'string',
+                                postal_code: 'string',
+                                country: 'string',
+                                latitude: -33.8798623,
+                                longitude: 151.2096914,
+                                localized_address_display: '70-72 Campbell St, Surry Hills NSW 2010',
+                                localized_area_display: 'string',
+                            },
+                            name: 'Soulrap Bar',
+                            latitude: -33.8798623,
+                            longitude: 151.2096914,
+                        },
+                    // tslint:disable-next-line:no-any
+                    } as any,
+                    ...events,
+                ]),
                 share(),
             );
     }
