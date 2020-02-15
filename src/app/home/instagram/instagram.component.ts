6c24e5b0d1207865f2176132ab5ebe6dbd754bbb
Dolan Miu <dolan_miu@hotmail.com>
2020-02-15 21:58:38 +0000

    Upgrade to HttpClient

--- src/app/home/instagram/instagram.component.ts
+++ src/app/home/instagram/instagram.component.ts
@@ -1,5 +1,5 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -11,11 +11,10 @@ import { map } from 'rxjs/operators';
 export class InstagramComponent implements OnInit {
     public photos$: Observable<InstagramPhotoData[]>;
 
-    constructor(http: Http) {
-        this.photos$ = http.get(`https://wt-9017166451e5dc00461b648d19f5e8da-0.sandbox.auth0-extend.com/sydney-chamber-instagram`).pipe(
-            map((res) => res.json() as InstagramPhotoResponse),
-            map((res) => res.data),
-        );
+    constructor(http: HttpClient) {
+        this.photos$ = http
+            .get<InstagramPhotoResponse>(`https://wt-9017166451e5dc00461b648d19f5e8da-0.sandbox.auth0-extend.com/sydney-chamber-instagram`)
+            .pipe(map((res) => res.data));
     }
 
     public ngOnInit(): void {}
