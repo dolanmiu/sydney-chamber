af756185b5b2448d306445fbc30b112a17600065
Dolan Miu <dolan_miu@hotmail.com>
2020-10-27 22:05:57 +0000

    Fix youtube async issue

--- src/app/home/discography/discography.component.ts
+++ src/app/home/discography/discography.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./discography.component.scss'],
 })
 export class DiscographyComponent implements OnInit {
+    private apiLoaded = false;
+
     constructor() {}
 
-    public ngOnInit(): void {}
+    public ngOnInit(): void {
+        if (!this.apiLoaded) {
+            // This code loads the IFrame Player API code asynchronously, according to the instructions at
+            // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
+            const tag = document.createElement('script');
+            tag.src = 'https://www.youtube.com/iframe_api';
+            document.body.appendChild(tag);
+            this.apiLoaded = true;
+        }
+    }
 }
